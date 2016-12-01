9c8b98606eb418a247f0feb53a498ec3d7b07cc5
subhashreemangaraj <subhashree.202@gmail.com>
2016-12-01 09:23:05 +0530

    Assignment3_3

---
+++ Assignment3_3.java
@@ -0,0 +1,16 @@
+
+public class Assignment3_3 {
+	
+	int x; int y;
+	 String name;
+	 
+	 public static void main(String args[]) {
+		 
+		 Assignment3_3 pnt = new Assignment3_3();
+	 
+	 System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+	 }
+
+}
+
+//output: pnt is null 0 0
